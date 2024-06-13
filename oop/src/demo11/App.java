08cdab454694ffba8f7f66071dfc5b64a591e7ae
jhta <gyulbbe@gmail.com>
2024-06-13 11:32:56 +0900

    오버라이딩

---
+++ oop/src/demo11/App.java
@@ -0,0 +1,12 @@
+package demo11;
+
+public class App {
+
+	public static void main(String[] args) {
+		Printer p1 = new Printer();
+		p1.print();
+		
+		ColorPrinter p2 = new ColorPrinter();
+		p2.print();
+	}
+}
