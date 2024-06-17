bd0ec4e03a5b891c9c13e64c448f19321693f451
jhta <gyulbbe@gmail.com>
2024-06-17 14:03:39 +0900

    추상화

---
+++ oop/src/demo17/Job1.java
@@ -0,0 +1,9 @@
+package demo17;
+
+public class Job1 extends AbstractJob {
+
+	@Override
+	public void job() {
+		System.out.println("핵심작업1 실행됨");
+	}
+}
---
+++ oop/src/demo17/Job2.java
@@ -0,0 +1,9 @@
+package demo17;
+
+public class Job2 extends AbstractJob {
+
+	@Override
+	public void job() {
+		System.out.println("핵심작업2 실행됨");
+	}
+}
